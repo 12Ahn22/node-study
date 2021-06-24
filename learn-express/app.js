f98b8e1e1c97cd51207e4bf6b62122d9554799f0
ayo <12ayo22@gmail.com>
2021-06-24 21:40:44 +0900

    multer 미들웨어 사용해보기

--- learn-express/app.js
+++ learn-express/app.js
@@ -10,6 +10,36 @@ const path = require('path');
 // process.env를 관리하기 위한 패키지
 const dotenv = require('dotenv');
 
+// multer
+const multer = require('multer');
+// multer 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req ,file ,done){
+      // req와 file을 가공해서 done 함수로 넘긴다
+      // done(에러, 실제 경로나 파일이름);
+      done(null,'uploads/'); // 파일이 저장되는 위치
+    },
+    filename(req ,file ,done){ // 파일이 저장되는 이름
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  // 업로드에 대한 제한 사항
+  limits:{ fileSize: 5 * 1024 * 1024 },
+});
+
+// 파일시스템 모듈
+const fs = require('fs');
+
+// 파일 시스템으로 multer를 위한 폴더 만들기
+try {
+  fs.readdirSync('uploads'); // 디렉터리 읽기
+} catch (error) {
+  console.log('uploads 폴더가 없어서 폴더를 생성합니다');
+  fs.mkdirSync('uploads'); // 없다면 생성하기
+}
+
 // dotenv
 dotenv.config();
 
@@ -19,7 +49,17 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 // 미들웨어 패키지들 사용해보기
-app.use(morgan('dev')); // 로그를 콘솔에 찍는 미들웨어
+// app.use(morgan('dev')); // 로그를 콘솔에 찍는 미들웨어
+// morgan 미들웨어를 미들웨어 패턴을 이용해 분기처리 해보기
+app.use((req,res,next)=>{
+  if(process.env.NODE_ENV === 'production'){
+    morgan('conbined')(req,res,next);
+  }else{
+    morgan('dev')(req,res,next);
+  }
+});
+
+
 
 // 정적인 파일들을 제공하는 라우터 역할
 // localhost:3000/test.js
@@ -54,6 +94,18 @@ app.use((req, res, next) => {
   next(); // next를 해야 다음 일을 하기위해 넘어간다.
 });
 
+// 미들웨어 간에 데이터 전송하기
+app.use(
+  (req, res, next) => {
+    req.data = '다음 미들웨어에게 주고싶은 데이터';
+    next();
+  },
+  (req, res, next) => {
+    console.log('데이터를 받았나요?', req.data); // 위의 미들웨어에서 받은 데이터
+    next();
+  }
+);
+
 app.get(
   '/',
   (req, res, next) => {
@@ -66,6 +118,16 @@ app.get(
     // throw new Error('에러는 에러 처리 미들웨어로..'); // err.message
   }
 );
+
+// upload 관련 라우터
+// upload.single은 하나 업로드 받을 때
+// upload.array('many') 많은 업로드
+app.post('/upload',upload.single('image'),(req,res)=>{
+  // req.file에 내가 업로드한 데이터가 들어있다.
+  console.log(req.file, req.body);
+  res.send('OK');
+});
+
 // 에러 처리 미들웨어
 app.use((err, req, res, next) => {
   console.error(err);
